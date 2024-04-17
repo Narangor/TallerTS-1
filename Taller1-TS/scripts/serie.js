d087889c5e5a1d2960cc874f3a5581d5b4dc433d
Nicolás Arango <123480637+Narangor@users.noreply.github.com>
2024-04-17 09:46:24 -0500

    Add files via upload

---
+++ Taller1-TS/scripts/serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, name, channel, seasons, sinopsis, link, img) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.sinopsis = sinopsis;
+        this.link = link;
+        this.img = img;
+    }
+    return Serie;
+}());
+export { Serie };
---
+++ Taller1-TS/src/serie.ts
@@ -0,0 +1,13 @@
+export class Serie{
+
+    constructor(
+        public id: number,
+        public name: string,
+        public channel: string,
+        public seasons: number,
+        public sinopsis:string,
+        public link: string,
+        public img: string){
+
+        }
+}
